69c8f8ac797371e943f9aee2d50e24f2779b87bd
Karol Podsiadlik <karol@debian.str11>
2019-08-06 17:43:02 +0200

    poczatek programu

---
+++ calc1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void woisz(int &x, int &y){
+cout << "Podaj x" << endl;	
+cin >>x;	
+cout << "Podaj y" << endl;	
+cin >>y;	
+cout << "Podales x = "	 << x  << " y = " << y << endl;
+}
+
+
+int main(int argc, char** argv){
+	
+	
+	
+}	
+
+
