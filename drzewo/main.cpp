1021ab936d172878f9dd588fd5a3ae70f4405ae1
Karol Podsiadlik <karol@debian.str11>
2019-08-22 18:09:25 +0200

    drzewo

---
+++ drzewo/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+//narysuj drzewo kture bendzie sie wyswietlac
+
+int main(int argc, char** argv){
+//int i=0;	
+//for (;i<7;i++){
+//for (int s=(0);s<i;s++){
+//cout << " " ;
+//}
+//for (int j=7;j>i;j = j + 1){
+//cout << "#" ;
+//}
+//for (int j=7;j>i;j = j + 1){
+//cout << "#" ;	
+cout << "  ###" << endl;
+cout  << " #####" << endl;
+cout << "#######" << endl;
+cout << "$$$$$$$" << endl;
+cout << " $$$$$" << endl;
+cout << "  $$$" << endl;
+cout << "   $" << endl;
+cout << "   $" << endl;
+cout << "   $" << endl;
+cout << "   $" << endl;	
+cout << "########" << endl;
+ return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
