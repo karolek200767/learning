c1781dea2d3b9df2b3216175ba0246c4db0ec2f3
Karol Podsiadlik <karol@debian.str11>
2019-08-28 11:06:51 +0200

    dodany prostokont

--- drzewo/main.cpp
+++ drzewo/main.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 //narysuj drzewo kture bendzie sie wyswietlac
 
-int main(int argc, char** argv){
+int main(int argc, char** argv){	
+for (int i=0;i<11;i++){
+for (int j=0;j<7;j++){cout << "#" ;}
+cout << endl;
+}
+	
+				
 //int i=0;	
 //for (;i<7;i++){
 //for (int s=(0);s<i;s++){
@@ -12,20 +18,22 @@ int main(int argc, char** argv){
 //cout << "#" ;
 //}
 //for (int j=7;j>i;j = j + 1){
-//cout << "#" ;	
+//cout << "#	
+	
 cout << "  ###" << endl;
 cout  << " #####" << endl;
 cout << "#######" << endl;
-cout << "$$$$$$$" << endl;
-cout << " $$$$$" << endl;
-cout << "  $$$" << endl;
-cout << "   $" << endl;
-cout << "   $" << endl;
-cout << "   $" << endl;
-cout << "   $" << endl;	
+cout << "#######" << endl;
+cout << " ######" << endl;
+cout << "  ###" << endl;
+cout << "   #" << endl;
+cout << "   #" << endl;
+cout << "   #" << endl;
+cout << "   #" << endl;	
 cout << "########" << endl;
- return 0; 
-}
+
+return 0; 
+}  
 
 
 
