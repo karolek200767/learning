a6c1c564d676a628fb4fedbb98ada4b23ec4a4cb
Karol Podsiadlik <karol@debian.str11>
2019-10-05 11:50:35 +0200

    mini kalulator

---
+++ typy/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+char znak='5';	
+int liczba=5;	
+cout << znak+znak << endl;	
+cout <<	liczba+liczba <<endl;
+float cena=2499.99;
+cout << cena+cena << endl;
+cout << cena+liczba << endl;
+bool prawda=true;
+bool falsz=false;
+cout << prawda << endl;
+cout << falsz << endl;
+bool jestcieplo=false;
+if (jestcieplo){
+cout << "niemusimy ubierac kurtki" << endl;	
+}
+//int A=0;
+//int B=0;
+//cout << "podaj A" << endl;
+//cin >> A;
+//cout << "podaj B" << endl;
+//cin >> B;
+//cout << A+B << endl;
+
+//int D=0;
+//int E=0;
+//cout << "podaj D" << endl;
+//cin >> D;
+//cout << "podaj E" << endl;
+//cin >> E;
+//cout << D-E << endl;
+
+//int F=0;
+//int G=0;
+//cout << "podaj F" << endl;
+//cin >> F;
+//cout << "podaj G" << endl;
+//cin >> G;
+//cout << F*G << endl;
+
+//int H=0;
+//int J=0;
+//cout << "podaj H" << endl;
+//cin >> H;
+//cout << "podaj J" << endl;
+//cin >> J;
+//cout << H/J << endl;
+
+float A=0;
+float B=0;
+cout << "podaj A" << endl;
+cin >> A;
+cout << "podaj B" << endl;
+cin >> B;
+cout << A+B << endl;     
+float D=0;
+float E=0; 
+cout << "podaj D" << endl;
+cin >> D;
+cout << "podaj E" << endl;
+cin >> E;
+cout << D-E << endl;
+float F=0;
+float G=0;
+cout << "podaj F" << endl;
+cin >> F;
+cout << "podaj G" << endl;
+cin >> G;
+cout << F*G << endl;
+float H=0;
+float J=0;
+cout << "podaj H" << endl;
+cin >> H;
+cout << "podaj J" << endl;
+cin >> J;
+cout << H/J << endl; 
+
+return 0;	
+}
+
+
