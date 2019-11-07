e994db8a7cb22a0b93ff8769284cdaa025c942cf
Karol Podsiadlik <karol@debian.str11>
2019-11-07 16:50:45 +0100

    dodawanie pkt

--- wysokosc/main.cpp
+++ wysokosc/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {	
-int wys, punkty_karne;	
+int wys, punkty_karne=0;	
 while (wys>0){
 std::cout <<  " Na jakiej wysokosci lecimy ? [w metrach]: ";
 std::cin >>wys;
@@ -15,15 +15,18 @@ if(wys ==1){ cout <<" metr ";}
 if((wys >1)&&(wys <5) ){cout<< " metry ";}
 if(wys >=5){cout<<" metrow "; }
 cout << " to za nisko !\n ";
-punkty_karne = 1;
+punkty_karne =punkty_karne +1;
 }
 else
 {
 std::cout<<"\n"<< wys <<" metrow jestes juz bezpieczny \n ";
 punkty_karne = 0;
 }
-if(punkty_karne >=2 ){std::cout<<" Masz "<< punkty_karne;} 
-//if(punkty_karne ,){cout<<" punkty " ;} 
+std::cout<<" Masz "<< punkty_karne; 
+if (punkty_karne ==1){cout<<" punkt karny" ;} 
+if (punkty_karne >1){cout<<"punkty karne" ;} 
+if (punkty_karne >=5){cout<<"punktow karnych";}
+
 //if(){cout << "karny \n ";}
 if(punkty_karne) std::cout<<" Popraw sie ! ";
 
@@ -31,16 +34,3 @@ if(punkty_karne) std::cout<<" Popraw sie ! ";
 }
 return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
