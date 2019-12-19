0ba6071eddf508696fe033d61f8de943ccd6ec7a
Karol Podsiadlik <karol@debian.str11>
2019-12-19 16:48:41 +0100

    Tablice

--- tablice/main.cpp
+++ tablice/main.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int main()
 {
-int t[4];	
-for(int i = 0 ;i < 4 ;i++)
-t[i] = 100 * i;
+int wielkosctablicy = 4;
+int t[wielkosctablicy];
+for(int i = 0 ;i < wielkosctablicy ;i++){	
+cout <<  "wpisz liczbe" << endl;
+cin >> t [i];
+}
+//t[i] = 200 * i;
 cout<<"wydruk tresci tablicy: \n";
-for(int i = 0;i < 4 ;i++)
+for(int i = 0;i < wielkosctablicy ;i++)
 {
 cout <<"Element nr:" << i << "ma wartosc" << t[i] << endl;	
 }
 return 0;	
 }
+	
