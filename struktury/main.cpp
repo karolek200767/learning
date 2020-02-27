b5f1bc26a40aeaaa18ddf926fae995416b69298a
Karol Podsiadlik <karol@debian.str11>
2020-02-27 16:59:33 +0100

    funkcje size lenght

--- struktury/main.cpp
+++ struktury/main.cpp
@@ -7,7 +7,7 @@ int rok;
 
 	
 };
-
+int maks=0;
 film t[100];
 int main(){
 film f1;
@@ -22,12 +22,23 @@ cout <<"urzytkowniku podaj rok"<<  endl;
 cin >> t[i].rok;
 cout <<"nazwa filmu: "<< endl;
 }
+
+for (int i = 0;i < filmow ;i++){
+int tmp=t[i].nazwa.length();
+//if jezeli tmp jest wienkszy od maks to maks=tmp;
+if (tmp>maks){
+maks=tmp;
+cout << maks << endl;	
+ }
+}
+cout << "najdłurzszy ma"<< endl;
+cout << maks << endl;
 cout <<" L.P| NAZWA | ROK "<< endl;
-cout <<" ---------------- "<< endl;
-	
+cout <<" ---------------- "<< endl;	
 for (int i = 0;i < filmow ;i++){
 cout <<"  |  " << t[i].nazwa <<"  |  "<< t[i].rok << endl;
 }
-												
+cout <<"najwiencej znakow to"<< endl;
+											
 return 0;	
 }
