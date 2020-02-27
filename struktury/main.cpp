c21f6d3cd1157c5342a83a99d2df21e512f21060
Karol Podsiadlik <karol@debian.str11>
2020-02-27 16:07:41 +0100

    struktury skonczone

--- struktury/main.cpp
+++ struktury/main.cpp
@@ -11,43 +11,23 @@ int rok;
 film t[100];
 int main(){
 film f1;
+int filmow = 0;
+cout <<"podaj ile chcesz wpisac filmow "<< endl;
+cin>> filmow;
+for (int i = 0;i < filmow ;i++)
+{
 cout <<"urzytkowniku podaj nazwe filmu"<< endl;
-cin >> t[0].nazwa;
+cin >> t[i].nazwa;
 cout <<"urzytkowniku podaj rok"<<  endl;
-cin >> t[0].rok;
+cin >> t[i].rok;
 cout <<"nazwa filmu: "<< endl;
-
-cout <<"urzytkowniku podaj nazwe filmu"<< endl;
-cin >> t[1].nazwa;
-cout <<"urzytkowniku podaj rok"<<  endl;
-cin >> t[1].rok;
-cout <<"nazwa filmu: "<< endl;
-
-cout <<"urzytkowniku podaj nazwe filmu"<< endl;
-cin >> t[2].nazwa;
-cout <<"urzytkowniku podaj rok"<<  endl;
-cin >> t[2].rok;
-cout <<"nazwa filmu: "<< endl;
-
-cout <<"urzytkowniku podaj nazwe filmu"<< endl;
-cin >> t[3].nazwa;
-cout <<"urzytkowniku podaj rok"<<  endl;
-cin >> t[3].rok;
-cout <<"nazwa filmu: "<< endl;
-
-cout <<"urzytkowniku podaj nazwe filmu"<< endl;
-cin >> t[4].nazwa;
-cout <<"urzytkowniku podaj rok"<<  endl;
-cin >> t[4].rok;
-cout <<"nazwa filmu: "<< endl;
-
+}
 cout <<" L.P| NAZWA | ROK "<< endl;
 cout <<" ---------------- "<< endl;
-cout <<"  |  " << t[0].nazwa <<"  |  "<< t[0].rok << endl;
-cout <<"  |  " << t[1].nazwa <<"  |  "<< t[1].rok << endl;
-cout <<"  |  " << t[2].nazwa <<"  |  "<< t[2].rok << endl;
-cout <<"  |  " << t[3].nazwa <<"  |  "<< t[3].rok << endl;
-cout <<"  |  " << t[4].nazwa <<"  |  "<< t[4].rok << endl;
-													
+	
+for (int i = 0;i < filmow ;i++){
+cout <<"  |  " << t[i].nazwa <<"  |  "<< t[i].rok << endl;
+}
+												
 return 0;	
 }
