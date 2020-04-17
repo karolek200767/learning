073930cf5a0400cbf1176159f7a5d4269533ddef
Karol <karolpodsiadlik@gmail.com>
2020-04-17 18:00:41 +0200

    zapisywanie do pliki

--- struktury/main.cpp
+++ struktury/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include<fstream>
 using namespace std;
+ofstream zapis("dane.txt");//obiekt tworzymy tylko raz
+
 struct film{
 string nazwa;
 int rok;
@@ -9,9 +12,27 @@ int rok;
 int maks=0;
 film t[100];
 int dlugosc [100];
-int main(){
 film f1;
 int filmow = 0;
+
+
+int podajrok(){
+int wprowadzona = 0;
+cin >> wprowadzona;
+if (wprowadzona > 1000){
+if (wprowadzona < 3000){
+return wprowadzona;	
+}else{
+cout <<"podana liczba jest za du¿a";	
+}
+
+}else{
+cout <<"podana liczba jest za mala";
+};
+	
+return podajrok();	
+}
+void podawanie(){
 cout <<"podaj ile chcesz wpisac filmow "<< endl;
 cin>> filmow;
 for (int i = 0;i < filmow ;i++)
@@ -19,9 +40,14 @@ for (int i = 0;i < filmow ;i++)
 cout <<"urzytkowniku podaj nazwe filmu"<< endl;
 cin >> t[i].nazwa;
 cout <<"urzytkowniku podaj rok"<<  endl;
-cin >> t[i].rok;
-cout <<"nazwa filmu: "<< endl;
-}
+//cin >> t[i].rok;
+t[i].rok = podajrok();
+//cout <<"nazwa filmu: "<< endl;
+ }
+
+} 
+
+void obliczaniemaks(){
 for (int i = 0;i < filmow ;i++){
 int tmp=t[i].nazwa.length();
 //if jezeli tmp jest wienkszy od maks to maks=tmp;
@@ -37,7 +63,15 @@ dlugosc[i] = t[i].nazwa.length();
 
 cout << "najd³urzszy ma"<< endl;
 cout << maks << endl;
-cout <<" L.P| NAZWA | ROK "<< endl;
+
+}
+
+void wyswietlanie(){
+cout <<"L.P| NAZWA";
+for (int k = 0;k < maks-3 ;k++){
+	cout<< " " ;
+}
+cout<< "| ROK "<< endl;
 for (int pl = 0;pl < maks + 20 ;pl++){
 cout << "-" ;	
 } 
@@ -59,7 +93,42 @@ cout <<"  |  "
 << dlugosc [i] << endl;
 }
 
-return 0;
- 
+}
+
+void zapisywanie(){
+zapis <<"L.P| NAZWA";
+for (int k = 0;k < maks-3 ;k++){
+	zapis<< " " ;
+}
+zapis<< "| ROK "<< endl;
+for (int pl = 0;pl < maks + 20 ;pl++){
+zapis << "-" ;	
+} 
+zapis << endl;	
+for (int i = 0;i < filmow ;i++){
+int lp = i + 1;
+zapis << lp <<"  |  " ;
+int ile = maks-dlugosc[i];
+
+zapis << t[i].nazwa ;  
+
+for (int k = 0;k < ile ;k++){
+	zapis<< " " ;
+}
+
+zapis <<"  |  "
+<< t[i].rok
+<< " | "
+<< dlugosc [i] << endl;
+}
+
+zapis.close(); //obowi¹zkowo nale¿y zamkn¹æ plik
+}
+int main(){	
+podawanie();
+obliczaniemaks();
+wyswietlanie();
+zapisywanie();										
+return 0;	
 }
 
