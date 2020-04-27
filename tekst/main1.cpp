e268d892db958b8084e9aec5666ceda23c4ae090
Karol <karolpodsiadlik@gmail.com>
2020-04-27 17:57:16 +0200

    nie dokończiona praca

---
+++ tekst/main1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+
+void plakat(string s);
+
+int main(){
+	
+string klub1("Wisla");
+string klub2("Unia");
+
+string mias1("Karakow");
+string mias2 ("Tarnow");
+
+string polaczenie;
+polaczenie = klub1 + klub2;
+cout <<"Dodanie dowch stringow["<< polaczenie << "]\n\n";
+
+string zapowiedz = klub1 + "-" + mias1;
+
+zapowiedz = zapowiedz + "vs.";
+
+zapowiedz += klub2;
+zapowiedz += ('-' + mias2);
+
+cout << "!!! Zapraszamy na mecz !!!\n";
+plakat(zapowiedz);
+
+zapowiedz +="  Rezultat meczu";
+
+cout << "\n\nPodaj wynik meczu";
+string wynik;
+cin >> wynik;
+
+cout << "\n\nUWAGA: wczoraj odbyl sie mecz\n";
+
+plakat (zapowiedz + wynik);
+return 0;
+}
+void plakat(string tresc){
+	
+string gwiazdki(tresc.length() + 6,'*');
+cout << gwiazdki <<'\n'<< gwiazdki << "\r"<< "** " << tresc << " **\n"<< gwiazdki << endl;
+}
+
+void dopliku(){
+string gwiazdki(tresc.length() + 6,'*');
+ofstream zapis("dane.txt");
+zapis << gwiazdki <<'\n'<< gwiazdki << "\r"<< "** " << tresc << " **\n"<< gwiazdki << endl;	
+}
