4464ef63bbf08311a0d0ce41a22c4dc29e352091
Karol Podsiadlik <karolpodsiadlik@gmail.com>
2021-10-19 18:13:19 +0200

    added tree project

---
+++ newTree/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+for (int i=0;i<6;i++){
+cout <<"" ;	
+
+for (int o=0;o<i;o++){
+cout <<"*" ;	
+	
+		}
+for (int p=5;p>0;p--){
+cout <<"&" ;	
+	}	
+cout << endl;		
+}
+
+
+	
+	
+return 0;
+}
