2ae2f295c71b11bae769abca2e36bff57c47047c
Karol Podsiadlik <karolpodsiadlik@gmail.com>
2021-10-19 18:42:26 +0200

    half cristmas tree

--- newTree/main.cpp
+++ newTree/main.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+int zmienna=4;
 	
-for (int i=0;i<6;i++){
-cout <<"" ;	
-
+for (int i=0;i<zmienna;i++){
+cout <<"" ;
+	
+for (int p=zmienna-i;p>0;p--){
+cout <<" " ;	
+		}
+		
 for (int o=0;o<i;o++){
 cout <<"*" ;	
 	
-		}
-for (int p=5;p>0;p--){
-cout <<"&" ;	
-	}	
+	}
+
 cout << endl;		
 }
 
