ca907ce1b3c09af034370557cf37eebc4a0178d4
Karol <you@example.com>
2022-04-08 16:35:54 +0200

    chinkaniedokończona

---
+++ Nowy projekt/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	
+cout<< "Karol Podsiadlik" << endl;	
+cout<< "Lat 15 oraz 1.75 wzrostu"<< endl; 
+		
+for (int y=0;y<10;y++){
+for (int t=0;t<y;t++)
+{
+cout <<"*" ;	
+}
+
+
+cout <<"      "<<endl;
+
+for (int e=0;e>0;e--)
+{
+cout <<"*";	
+}
+			
+		}
+
+	
+
+
+	return 0;
+}
