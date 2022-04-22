935774ebedbeb92d6d919d33e9474c4e7c0f5d3f
Karol <you@example.com>
2022-04-22 16:31:52 +0200

    kalkulatorv2

---
+++ kalkulatorv2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+char znak;
+int q=0, e=0;	
+
+cout<<"Podaj dzialanie"<<endl;
+
+cout<<"1.Dodawanie"<<endl;
+cout<<"2.Odejmowanie"<<endl; 
+cout<<"3.Mnozenie"<<endl; 
+cout<<"4.Dzielenie"<< endl;
+
+cin>>q;
+cin>>e;	
+
+
+if(znak){
+	
+cout<<"Dodawanie"<<endl; 
+
+cout<< "" << q << "+" << e << "=" << q+e << endl; 
+cin>> znak;
+}
+
+
+
+
+
+
+
+
+	
+	return 0;
+}
