6886e3e256374a4259a098c13a22d3bfd9ec8164
Karol <you@example.com>
2022-05-06 16:20:03 +0200

    nie dzielisz przez zero

--- kalkulatorv2/main.cpp
+++ kalkulatorv2/main.cpp
@@ -5,32 +5,55 @@ int main(int argc, char** argv) {
 char znak;
 int q=0, e=0;	
 
-cout<<"Podaj dzialanie"<<endl;
 
 cout<<"1.Dodawanie"<<endl;
 cout<<"2.Odejmowanie"<<endl; 
-cout<<"3.Mnozenie"<<endl; 
+cout<<"3.Mnozenie"<<endl;
 cout<<"4.Dzielenie"<< endl;
+cin>> znak; 
 
+cout<<"Podaj dzialanie"<<endl;
 cin>>q;
 cin>>e;	
 
 
-if(znak){
+if(znak == '1'){
 	
 cout<<"Dodawanie"<<endl; 
 
 cout<< "" << q << "+" << e << "=" << q+e << endl; 
-cin>> znak;
+
+}
+
+if(znak == '2'){
+	
+cout<<""<< q << "-" << e << "=" << q-e << endl;	
+}
+
+if(znak == '3'){
+	
+cout << "" << q << "*" << e << "=" << q*e << endl;	
+}
+
+if(znak == '4'){
+	
+if(e==0){
+	
+	std::cout << "nie mozna dzielic przez zero" << std::endl;	
+}else{
+
+	std::cout <<"nie dzielisz przez zero " << std::endl;
+	
+cout << "" << q << "/" << e << "=" << q/e << endl;
+	
+std::cout <<"Koniec" << endl; 	 
 }
 
 
 
+		
+}
 
 
-
-
-
-	
 	return 0;
 }
