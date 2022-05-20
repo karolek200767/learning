183902b35e3a9e50c1f7f4e7e8ef6bb262b7fae9
Karol <you@example.com>
2022-05-20 15:42:58 +0200

    nieskończony kalkulator

--- kalkulatorv2/main.cpp
+++ kalkulatorv2/main.cpp
@@ -6,18 +6,22 @@ char znak;
 int q=0, e=0;	
 
 
+while (znak != 'o'){
+
+cout<< "o. EXIT" <<endl; 
 cout<<"1.Dodawanie"<<endl;
 cout<<"2.Odejmowanie"<<endl; 
 cout<<"3.Mnozenie"<<endl;
 cout<<"4.Dzielenie"<< endl;
 cin>> znak; 
 
-cout<<"Podaj dzialanie"<<endl;
-cin>>q;
-cin>>e;	
 
 
 if(znak == '1'){
+	
+cout<<"Podaj dzialanie"<<endl;
+cin>>q;
+cin>>e;	
 	
 cout<<"Dodawanie"<<endl; 
 
@@ -27,15 +31,27 @@ cout<< "" << q << "+" << e << "=" << q+e << endl;
 
 if(znak == '2'){
 	
+cout<<"Podaj dzialanie"<<endl;
+cin>>q;
+cin>>e;	
+	
 cout<<""<< q << "-" << e << "=" << q-e << endl;	
 }
 
 if(znak == '3'){
 	
+cout<<"Podaj dzialanie"<<endl;
+cin>>q;
+cin>>e;	
+	
 cout << "" << q << "*" << e << "=" << q*e << endl;	
 }
 
 if(znak == '4'){
+	
+cout<<"Podaj dzialanie"<<endl;
+cin>>q;
+cin>>e;	
 	
 if(e==0){
 	
@@ -49,11 +65,9 @@ cout << "" << q << "/" << e << "=" << q/e << endl;
 std::cout <<"Koniec" << endl; 	 
 }
 
+			}
 
-
-		
 }
-
 
 	return 0;
 }
